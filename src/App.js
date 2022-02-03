74e426d128120331e2b28b447f1ec47e06b8e3b0
mikotalik <mikotalik@seznam.cz>
2022-02-03 15:59:00 +0100

    Not working: producing byte order errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { readRasterFromURL } from 'fast-geotiff'
+//import
+
+console.log("starting...")
+const imageData = readRasterFromURL('nasa.tif')
+
+class Map extends React.Component{
+  constructor(props){
+    super(props);
+    //this.state = {text : props.text};
+  }
+
+  render(){
+    return(
+      <div className="map">
+      <h2> {this.props.text} </h2>
+      </div>
+    );
+  }
+}
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Testing the Geotiff import </h1>
+      <Map text={"Here will be a map"}/>
     </div>
   );
 }
