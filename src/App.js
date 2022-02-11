35bfe125d716e6b2ddf5f32b5b1e1d8b9991a4f2
mikotalik <mikotalik@seznam.cz>
2022-02-11 18:01:49 +0100

    First barely working version

--- src/App.js
+++ src/App.js
@@ -10,31 +10,20 @@ import {StaticMap} from 'react-map-gl';
 import GeoTIFF, { fromUrl, fromUrls, fromArrayBuffer, fromBlob } from 'geotiff';
 import Sketch from "react-p5";
 
-
-//GLOBAL VARIABLES
 const url = "park.tif";
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoiam9ldmVjeiIsImEiOiJja3lpcms5N3ExZTAzMm5wbWRkeWFuNTA3In0.dHgiiwOgD-f7gD7qP084rg';
 
-let loaded = false;
-let image;
-let rgb;
-
-//THIS WORKS FINE
-async function getGeotiff(){
+async function getGeotiffData(dataUrl){
   let response;
   let arrayBuffer;
   let tiff;
+  let image;
 
-  response = await fetch(url);
-  console.log(response);
+  response = await fetch(dataUrl);
   arrayBuffer = await response.arrayBuffer();
-  console.log(arrayBuffer);
   tiff = await fromArrayBuffer(arrayBuffer);
-  console.log(tiff);
   image = await tiff.getImage(0);
-  console.log(image);
-  rgb = await image.readRGB()
-  console.log(rgb);
+  const rgb = await image.readRGB()
 
   const origin = image.getOrigin();
   const resolution = image.getResolution();
@@ -44,100 +33,110 @@ async function getGeotiff(){
   console.log("resolution " + resolution);
   console.log("bounding box " + bbox);
 
-  loaded = true;
+  return image;
 }
 
-getGeotiff();
+async function geoImg(geoTiffData){
+  const width = await geoTiffData.getWidth();
+  const height = await geoTiffData.getHeight();
+  const rgb = await geoTiffData.readRGB();
+  let colorBytes = await geoTiffData.getBytesPerPixel();
 
-//THE FUTURE IMG ELEMENT
-let snapshot;
+  let canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  let c = canvas.getContext('2d');
+  let imageData = c.createImageData(width, height);
 
-//THE OFFSCREEN CANVAS FOR P5 LIBRARY
-let board;
-let done = false;
 
-//SETUP RUNS ONCE. SET "draw" TO RUN ONCE A SECOND
-let setup = async (p5, canvasParentRef) => {
-  p5.frameRate(1);
-};
+  console.log("canvas width: " + width);
+  console.log("canvas height: " + height);
 
-//MEANWHILE DRAW RUNS EVERY SECOND. ONCE "getGeoTiff()"" sets "loaded" to true, parse the image
-//AND SET "SNAPSHOT" TO POINT TO THE NEWLY MADE IMG
-let draw = (p5) => {
-  if(loaded == true){
-    if(done == false){
-      board = p5.createGraphics(image.getWidth(),image.getHeight())
-      board.background(255);
-      board.loadPixels();
+  console.log("canvas pixels: " + imageData.data.length);
+  console.log("data pixels: " + rgb.length);
 
-      let offset = 0;
+  console.log("data bytes per pixel: " + colorBytes);
+  console.log("data samples per pixel: " + await geoTiffData.getSamplesPerPixel());
+  console.log("pixels: ");
+  //console.log(await geoTiffData.readRasters());
 
-      if(board.pixels.length > rgb.length){
-        for(let i = 0; i < board.pixels.length; i+=1){
-          if(i%4!=0){
-            offset += 1;
-          }
-          board.pixels[i] = rgb[offset];
-          if(i%4==0){
-            board.pixels[i-1] = 255;
-          }
-        }
-      }else if(board.pixels.length == rgb.length){
-        for(let i = 0; i < board.pixels.length; i+=1){
-          board.pixels[i] = rgb[i];
-        }
-      }
-
-      board.updatePixels();
-      snapshot = p5.createImg(board.elt.toDataURL());
-      console.log("image loaded: ");
-      console.log(snapshot);
-      done = true;
+  let offset = 0;
+  for(let i = 0; i < width*height*4; i+=1){
+    if(i%4!=0){
+      offset += 1;
+    }
+    imageData.data[i] = rgb[offset];
+    if(i%4==0){
+      imageData.data[i-1] = 255;
     }
   }
-};
+  c.putImageData(imageData,0,0);
+  let imageUrl = canvas.toDataURL('image/png');
+  return imageUrl;
+}
 
-//THE REACT COMPONENT DRAWING THE IMAGE STORED IN THE "snapshot" VARIABLE AND A MAP WITH THE SAME IMAGE ON IT.
+async function geotiffUrlToImg(address){
+  let data = await getGeotiffData(address);
+  let imgUrl = await geoImg(data);
+
+  data.bitmap = imgUrl;
+  return data;
+}
+
+class GeoImage{
+  image = new Image();
+  boundingBox = [0,0,10,10];
+
+  constructor(image, boundingBox){
+    this.image = image;
+    this.boundingBox = boundingBox;
+  }
+}
+
+async function getGeoImage(url){
+  let data = await getGeotiffData(url);
+  let imgUrl = await geoImg(data);
+
+  let boundingBox = data.getBoundingBox();
+
+  let g = new GeoImage(imgUrl, boundingBox);
+  return g;
+}
+
+//getGeoImage(url);
+
 class ImageMap extends React.Component{
   constructor(props){
     super(props);
-    this.state = {image, loaded};
+    this.state = {image : new Image(), boundingBox : [0,0,0,0]};
   }
 
-  componentDidUpdate(prevProps) {
-    // Typical usage (don't forget to compare props):
-    if (this.props.image !== prevProps.image) {
-      console.log("stuff changed");
-    }
+  async componentDidMount() {
+    const data = await getGeoImage(url);
+    this.setState({image: data.image, boundingBox: data.boundingBox});
   }
 
   render(){
-    const INITIAL_VIEW_STATE = {longitude: -122.41669,latitude: 37.7853,zoom: 13};
+    //let initial_view_state = {longitude: this.state.boundingBox[0],latitude: this.state.boundingBox[1],zoom: 12};
+    let initial_view_state = {longitude: -95.39842728115566,latitude: 29.763892665956423, zoom: 12};
 
-    //LAYERS FOR DECKGL. LineLayer TO DRAW A RANDOM LINE. BitmapLayer TO DRAW THE "snapshot" ON IT.
+    console.log("Bounding box: " + this.state.boundingBox);
+
     const layers = [
-      new LineLayer({id: 'line-layer', data: [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}]}),
-      new BitmapLayer({id: 'bitmap-layer', bounds: [-122.5190, 37.7045, -122.355, 37.829], image: this.props.image})
+      new BitmapLayer({id: 'bitmap-layer', bounds: this.state.boundingBox, image: this.state.image}),
+      new LineLayer({id: 'line-layer', data: [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}]})
     ];
-
-    console.log("ImageMap is rerendering");
-    console.log("Loaded image: ");
-    console.log(this.state.image);
 
     return(
       <div>
-      //DRAW THE IMAGE FROM PROPS (WHICH LOADS AT ANY TIME)
-      <img src = {this.props.image} />
-      //DRAW THE DECKGL COMPONENT CONTAINING THE "layers" VARIABLE. CORRECTLY LOADS LineLayer, NOT BirmapLayer WITH THE CORRECT IMAGE ("snapshot" variable)
       <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
+      initialViewState={initial_view_state}
       controller={true}
       layers={layers} >
       <MapView id="map" width="100%" height="70%" top="100px" controller={true}>
       <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
       </MapView>
       </DeckGL>
-      <h1> Map: </h1>
       </div>
     );
   }
@@ -148,10 +147,7 @@ function App() {
   return (
     <div className="App">
     <h1> DECK.GL GEOTIFF TEST </h1>
-    //RUN THE SKETCH (SETUP ONCE AND DRAW EVERY SECOND)
-    <Sketch setup={setup} draw={draw} className="P5" />
-    //RENDER THE IMAGEMAP COMPONENT, PASS THE "snapshot" CONTAINING THE IMAGE AND IF THE IMAGE IS LOADED
-    <ImageMap image={snapshot} loaded={done}/>
+    <ImageMap geoImage={new GeoImage}/>
     </div>
   );
 }
