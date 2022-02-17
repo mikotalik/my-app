9dddc69928e58a048db200b66bc37411452fa2e9
mikotalik <mikotalik@seznam.cz>
2022-02-17 10:40:14 +0100

    Added data clip feature, added comments, cleaned up.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,15 @@ import {MapView} from "@deck.gl/core"
 import {FirstPersonView} from "@deck.gl/core"
 import {Deck} from "@deck.gl/core"
 import {StaticMap} from 'react-map-gl';
-import GeoTIFF, { fromUrl, fromUrls, fromArrayBuffer, fromBlob } from 'geotiff';
+import {fromArrayBuffer} from 'geotiff';
 import Sketch from "react-p5";
+import {printMsg} from "geoimage"
 
 const url = "dsm.tif";
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoiam9ldmVjeiIsImEiOiJja3lpcms5N3ExZTAzMm5wbWRkeWFuNTA3In0.dHgiiwOgD-f7gD7qP084rg';
 
-console.clear();
-
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-const clamp = (num, min, max) => {
-  return Math.min(Math.max(num, max), min);
 }
 
 const heatmap = (num, min, max) => {
@@ -45,7 +40,6 @@ async function getGeotiffData(dataUrl){
 
   console.log("tiff origin " + origin);
   console.log("tiff resolution " + resolution);
-  console.log("tiff bounding box " + bbox);
 
   return image;
 }
@@ -55,30 +49,40 @@ async function geoImg(geoTiffData){
   const height = await geoTiffData.getHeight();
   const rasters = await geoTiffData.readRasters();
   const channels = rasters.length;
-  const colorBytes = await geoTiffData.getBytesPerPixel();
 
   let canvas = document.createElement("canvas");
   canvas.width = width;
   canvas.height = height;
   let c = canvas.getContext('2d');
   let imageData = c.createImageData(width, height);
-
   console.log("resolution: " + width + " * " + height);
-  console.log("data bytes per pixel: " + colorBytes);
-  console.log("data samples per pixel: " + await geoTiffData.getSamplesPerPixel());
 
+  //TRUE : COLOR RANGE WILL BE ADJUSTED FOR HIGHS AND LOWS OF THIS PICTURE
+  //FALSE : COLOR RANGE WILL BE ADJUSTED FOR RANGE MIN, RANGE MAX
+  const use_auto_range = true;
   let range_min = 0;
   let range_max = 255;
-  let color = [255,0,255];
-  let opacity = 150;
-  const use_auto_range = true;
+
+  //TRUE : DON'T RENDER ANYTHING WITH VALUE < CLIP_LOW OR VALUE > CLIP_HIGH
+  //FALSE : ALL VALUES WILL BE RENDERED
+  const use_clip = false;
+  const clip_low = 240;
+  const clip_high = Number.MAX_VALUE;
+
+  //TRUE : RENDER VALUES FROM LOWEST IN BLUE TO HIGHEST IN RED
+  //FALSE : RENDER VALUES IN THE SPECIFIED COLOR
   const use_heat_map = true;
+  let color = [255,0,255];
+
+  //TRUE : LOWS RENDER MORE TRANSPARENT. HIGHS RENDER MORE OPAQUE
+  //FALSE : DATA GETS RENDERED WITH THE SPECIFIED OPACITY
   const use_data_for_opacity = false;
+  let opacity = 150;
+
 
   let r,g,b,a;
 
   if(channels == 1){
-
     //AUTO RANGE
     if(use_auto_range){
       let highest = Number.MIN_VALUE;
@@ -91,18 +95,20 @@ async function geoImg(geoTiffData){
       }
       range_min = lowest;
       range_max = highest;
-      console.log("Auto-range enabled. Range min: " + range_min + ", max: " + range_max);
+      console.log("Auto-range enabled. Detected min: " + range_min + ", max: " + range_max);
     }
-    ///////////
 
     let pixel = 0;
-    for(let i = 0; i < width*height*4; i+=4){//MONOCHROME
+    for(let i = 0; i < width*height*4; i+=4){//MONOCHROME DATA
       if(use_heat_map) color = heatmap(rasters[0][pixel],range_min,range_max,0,255);
 
       r = color[0];
       g = color[1];
       b = color[2];
+
       a = opacity;
+
+      if((use_clip == true) && (rasters[0][pixel] < clip_low || rasters[0][pixel] > clip_high)) a = 0;
       if(use_data_for_opacity) a = scale(rasters[0][pixel],range_min,range_max,0,255);
 
       imageData.data[i+0] = r;
@@ -158,7 +164,7 @@ async function geotiffUrlToImg(address){
   return data;
 }
 
-class GeoImage{
+class GeoImg{
   image = new Image();
   boundingBox = [0,0,0,0];
 
@@ -168,13 +174,13 @@ class GeoImage{
   }
 }
 
-async function getGeoImage(url){
+async function getGeoImg(url){
   let data = await getGeotiffData(url);
   let imgUrl = await geoImg(data);
 
   let boundingBox = data.getBoundingBox();
 
-  let g = new GeoImage(imgUrl, boundingBox);
+  let g = new GeoImg(imgUrl, boundingBox);
   return g;
 }
 
@@ -185,15 +191,13 @@ class ImageMap extends React.Component{
   }
 
   async componentDidMount() {
-    const data = await getGeoImage(url);
+    const data = await getGeoImg(url);
     this.setState({image: data.image, boundingBox: data.boundingBox});
   }
 
   render(){
     let initial_view_state = {longitude: this.state.boundingBox[0],latitude: this.state.boundingBox[1],zoom: 12};
     //let initial_view_state = {longitude: -95.39842728115566,latitude: 29.763892665956423, zoom: 12};
-
-    console.log("Bounding box: " + this.state.boundingBox);
 
     const layers = [new BitmapLayer({id: 'bitmap-layer', bounds: this.state.boundingBox, image: this.state.image})];
 
@@ -203,7 +207,7 @@ class ImageMap extends React.Component{
       initialViewState={initial_view_state}
       controller={true}
       layers={layers} >
-      <MapView id="map" width="100%" height="70%" top="100px" controller={true}>
+      <MapView id="map" width="100%" height="100%" top="100px" controller={true}>
       <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
       </MapView>
       </DeckGL>
@@ -217,7 +221,7 @@ function App() {
   return (
     <div className="App">
     <h1> DECK.GL GEOTIFF TEST </h1>
-    <ImageMap geoImage={new GeoImage}/>
+    <ImageMap GeoImg={new GeoImg}/>
     </div>
   );
 }
